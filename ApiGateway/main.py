c8b34f549b816e70b1b86e2b7b906538dea27e7c
andref0716 <andref0716@gmail.com>
2022-11-22 11:10:47 -0500

    Creacion del token e implementacion del middleware

--- ApiGateway/main.py
+++ ApiGateway/main.py
@@ -9,6 +9,69 @@ import json
 app = Flask(__name__)
 cors = CORS(app)
 
+# LIBRERIAS JWT #
+
+from flask_jwt_extended import create_access_token, verify_jwt_in_request
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+# CREACION TOKEN #
+
+app.config["JWT_SECRET_KEY"] = 'top-secret' #puede llevar cualquier nombre
+jwt = JWTManager(app)
+
+
+@app.route("/login", methods=['POST'])
+def create_token():
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-security"]+"/usuarios/validar"
+    response = requests.post(url, json= data, headers=headers)
+    if response.status_code == 200:
+        user = response.json()
+        expires = datetime.timedelta(seconds=60*60*24)
+        access_token = create_access_token(identity=user,
+            expires_delta=expires)
+        return jsonify({"token": access_token,
+            "user_id": user["_id"]})
+    else:
+        return jsonify({
+            "Message": "correo o contraseña incorrecto. "
+        }), 401  
+
+
+# MIDDLEWARE #
+
+def limpiarUrl(url):
+    partes = request.path.split("/")
+    for laParte in partes:
+        if re.search('\\d', laParte): 
+           url = url.replace(laParte, "?")
+
+    return url
+
+
+def validarPermiso(endPoint, metodo, idRol):
+    url = dataConfig["url-backend-security"]+"/permisos-roles/validar-permiso/rol/"+str(idRol)
+    tienePermiso= False
+    headers= {"Content-Type": "application/json; charset=utf-8"}
+    body = {
+        "url": endPoint,
+        "metodo": metodo
+    }
+    response = requests.get(url, json=body, headers=headers)
+    try:
+        data = response.json()
+        if("_id" in data):
+            tienePermiso =True
+    except: 
+        pass
+    return tienePermiso   
+
+
+
+
 #  TEST DE SERVICIO # 
 
 @app.route("/", methods=['GET'])
